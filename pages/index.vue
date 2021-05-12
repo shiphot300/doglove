6343b0c696e0cff820b1960f72251f7ae315b8be
youngjae <shiphot300@naver.com>
2021-05-12 11:47:46 +0900

    add doglove

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,11 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        doglove
-      </h1>
-      <h2 class="subtitle">
-        Dogs and cats 
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+<main>
+  <div>강아지와 고양이를 위한 웹사이트(내가 앞으로 만들 웹사이트에 대한 주제 적어놓을것</div>
+  <div><h2>강아지</h2><br>강아지는 귀여웡</div>
+</main>
+  
+  
 </template>
 
-<script>
-import AppLogo from '~/components/AppLogo.vue'
 
-export default {
-  components: {
-    AppLogo
-  }
-}
-</script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
 
---
+++ pages/upload.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "add doglove "
+git push -u origin master
