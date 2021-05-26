a277a6f40d69f2fcf358086e46392d26caeff42e
youngjae <shiphot300@naver.com>
2021-05-26 11:46:39 +0900

    add myblog

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
+    <nav class="breadcrumb"></nav>
+      <ul>
+        <li><nuxt-link to="/"><i class="fas fa-home"></i>홈으로</nuxt-link></li>
+      </ul>
     <nuxt/>
+    <footer class="footer">
+  <div class="content has-text-centered">
+    <i class="fas fa-copyright"></i>2021 모든 저작권은 <strong> 나의 블로그 </strong>에 있습니다.
+    
+  </div>
+</footer>
   </div>
 </template>
 
--- upload.bat
+++ upload.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "[rename] fontaswom"
+git commit -m "add myblog "
 git push -u origin master
