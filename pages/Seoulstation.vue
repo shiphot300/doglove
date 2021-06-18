fa8d9003999db2d2d0a09d89d89265fad2b21d0e
youngjae <shiphot300@naver.com>
2021-06-18 16:50:27 +0900

    fix station.json

--- pages/Seoulstation.vue
+++ pages/Seoulstation.vue
@@ -15,19 +15,21 @@
             <div class="columns">
                 <table class="table">
                     <thead>
-                        <th>번호</th>
+                        <th>00호선</th>
                         <th>도시</th>
+                        <th>코드</th>
+                        <th>위도</th>
                         <th>좌표</th>
                     </thead>
                     <tbody>
-                        <template v-for ="pos in tablekoreanKeys.length">
-                            <tr :key="pos">
-                                <td>{{ pos }}</td>
-                                <td>{{ tablekoreanKeys[pos -1] }}</td>
-                                <td>
-                                    <span class="tag is-black" v-if="ar =tablekorean [tablekoreanKeys[pos -1]], ar.length ==0">좌표</span>
-                                    <span v-else><template v-for="subbreed in ar"><span class="tag is-success" :key="subbreed">{{subbreed}}</span>&nbsp;</template></span>
-</td>
+                        <template v-for ="(item,index) in tablekorean">
+                            <tr :key="index">
+                                <td>{{item.line }}</td>
+                                <td>{{item.name}}</td>
+                                <td><span class="tag is-warning" >{{item.code}}</span></td>
+                                <td><span class="tag is-success" >{{item.lat}}</span></td>
+                                <td><span class="tag is-danger" >{{item.lng}}</span></td>
+
 </tr>
 </template>
 </tbody>
@@ -39,7 +41,7 @@
     import axios from 'axios';
     export default {
         async asyncData() {
-            const koreancity = await axios.get('https://gist.githubusercontent.com/hmmhmmhm/b3a950e84f865f8abbd00fa460aa4613/raw/84c0bbdcfc0ef96f6841bd5670835beb4756dae6/sigungu.json');
+            const koreancity = await axios.get('https://raw.githubusercontent.com/shiphot300/doglove/master/assets/station_coordinate.json');
             return {
                 tablekorean: koreancity.data,
                 tablekoreanKeys: Object.keys(koreancity.data),
